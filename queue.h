c88cdbe213b04b72ad38ecf2389833d04daf5b09
gandhiboi <krijestorac.kenan@gmail.com>
2021-03-19 01:59:39 -0500

    implemented a queue

---
+++ queue.h
@@ -0,0 +1,21 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	unsigned int front, rear, size;
+	unsigned int capacity;
+	unsigned int* array;
+} Queue;
+
+Queue* createQueue(unsigned int);
+unsigned int isFull(Queue*);
+unsigned int isEmpty(Queue*);
+void enqueue(Queue*, unsigned int);
+unsigned int dequeue(Queue*);
+unsigned int front(Queue*);
+unsigned int rear(Queue*);
+
+#endif
